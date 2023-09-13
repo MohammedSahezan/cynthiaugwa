b5721a567a453c1ce6ac355a8050da58ae826ec0
MohammedSahezan <patelsahezan28@gmail.com>
2023-09-13 13:51:05 +0530

    ADD new js file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # cynthiaugwa
-This is a  simple Cynthiaugwa clone written in  HTML,CSS and cs. <br>
-Author - patel MohammedSahezan 
+This is a  simple Cynthiaugwa clone written in  HTML,CSS and JS. <br>
+Author - Patel MohammedSahezan 
---
+++ script.js
@@ -0,0 +1,18 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
+
+
+
+
+let time = document.getElementById("timezone");
+setInterval(() => {
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+}, 1000);
+
